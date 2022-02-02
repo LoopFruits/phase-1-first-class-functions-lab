61384d01e8215cb0bd8dc2da11e02f265bdf1c6f
LoopFruits <Kensworldc@gmail.com>
2022-02-02 12:09:30 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+// Code your solution in this fil
+
+const returnFirstTwoDrivers = function(drivers){
+    return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function(drivers){
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(math){ // created a function called createFareMultiplier that takes an argument math
+    return function createFareMultiplier(fare){ //returning a function that will multiply a fare
+            return fare * math;
+    }
+}
+
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+
+selectDifferentDrivers = (arrayOfDrivers, returnLastTwoDrivers) => {
+    return returnLastTwoDrivers(arrayOfDrivers);
+}
